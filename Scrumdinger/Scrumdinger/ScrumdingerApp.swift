fa9ae64ddb518931de5551a8270fdea0cf1ce29c
DannyGrimmig <dannygrimmig17@gmail.com>
2022-09-19 23:01:43 -0400

    chapter 1

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Danny Grimmig on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
